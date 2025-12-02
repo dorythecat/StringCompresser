8a31473ecd2619d4dd0c248ebc1e7c4f99b55443
Dory <pemegomachado@gmail.com>
2025-12-02 17:10:04 +0100

    Added basic code

---
+++ main.py
@@ -0,0 +1,9 @@
+in_str: str = input("Enter a string: ")
+
+print(f"You entered: {in_str}")
+print(f"Length of the string: {len(in_str)}")
+
+out_str: str = in_str
+
+print(f"Encoded string: {out_str}")
+print(f"Length of encoded string: {len(out_str)}")
