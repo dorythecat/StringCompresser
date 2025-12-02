c2076626e74ff880ecfa20c86beea587c5cf9be6
Dory <pemegomachado@gmail.com>
2025-12-02 17:12:29 +0100

    Made main.py proper

--- main.py
+++ main.py
@@ -3,7 +3,14 @@ in_str: str = input("Enter a string: ")
 print(f"You entered: {in_str}")
 print(f"Length of the string: {len(in_str)}")
 
-out_str: str = in_str
+def encode(string: str) -> str:
+    return string
 
-print(f"Encoded string: {out_str}")
-print(f"Length of encoded string: {len(out_str)}")
+def decode(string: str) -> str:
+    return string
+
+print(f"Encoded string: {encode(in_str)}")
+print(f"Length of encoded string: {len(encode(in_str))}")
+
+print(f"Decoded string: {decode(encode(in_str))}")
+print(f"Length of decoded string: {len(decode(encode(in_str)))}")
