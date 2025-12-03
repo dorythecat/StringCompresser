d4a79462bcad7dfe0412c07abd03c3f55d98d3b7
Dory <pemegomachado@gmail.com>
2025-12-03 17:08:28 +0100

    Changed encoding to ASCII, to avoid some weird edge-cases

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ print(f"You entered: {in_str}")
 print(f"Length of the string: {len(in_str)}")
 
 def encode(s: str) -> str:
-    v = int(s.encode('utf-8').hex(), 16)
+    v = int(s.encode('ascii').hex(), 16)
     enc: str = ''
     while v > 0:
         v, r = divmod(v, 1114111)
@@ -16,7 +16,7 @@ def decode(s: str) -> str:
     for c in s:
         v = v * 1114111 + ord(c)
     r = str(hex(v))[2:]
-    return str(bytes.fromhex('0' * (len(r) % 2) + r).decode('utf-8'))
+    return str(bytes.fromhex('0' * (len(r) % 2) + r).decode('ascii'))
 
 print(f"Encoded string: {encode(in_str)}")
 print(f"Length of encoded string: {len(encode(in_str))}")
