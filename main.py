0a8087d7bb1b3a6f619770d9996786411b4f09ad
Dory <pemegomachado@gmail.com>
2025-12-03 16:39:20 +0100

    Made new mod value be 65536

--- main.py
+++ main.py
@@ -7,14 +7,14 @@ def encode(s: str) -> str:
     v = int(s.encode('utf-8').hex(), 16)
     enc = ''
     while v > 0:
-        v, r = divmod(v, 2048)
+        v, r = divmod(v, 65536)
         enc = chr(r) + enc
     return enc
 
 def decode(s: str) -> str:
     v = 0
     for c in s:
-        v = v * 2048 + ord(c)
+        v = v * 65536 + ord(c)
     r = str(hex(v))[2:]
     return bytes.fromhex('0' * (len(r) % 2) + r).decode('utf-8')
 
